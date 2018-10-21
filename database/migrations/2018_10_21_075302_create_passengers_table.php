4b7999e453e2d5bfb9194e2b4db2d87ad7239beb
Pedro Gomes <pedrogomesyoo@gmail.com>
2018-10-21 11:17:17 +0200

    Add Passenger table migration

---
+++ database/migrations/2018_10_21_075302_create_passengers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePassengersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passengers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('survived');
+            $table->string('pclass');
+            $table->string('name');
+            $table->string('sex');
+            $table->integer('age');
+            $table->integer('siblings_spouses_aboard');
+            $table->integer('parents_children_aboard');
+            $table->float('fare', 12, 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passengers');
+    }
+}
