12e80576d65bbfa13c0031330f82c3a780d3845e
Pedro Gomes <pedrogomesyoo@gmail.com>
2019-05-30 18:59:53 +0000

    rm unused module

--- load_test.js
+++ load_test.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import { check, group, sleep } from "k6";
+import { check, sleep } from "k6";
 import { Rate } from "k6/metrics";
 
 // A custom metric to track failure rates
